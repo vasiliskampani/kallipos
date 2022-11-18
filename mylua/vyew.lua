b45acc94615d6a2c6e8ab5d8821ddadd44fd66ff
vasiliskampani <72518208+vasiliskampani@users.noreply.github.com>
2022-11-19 00:48:16 +0200

    update my filter lua

--- mylua/vyew.lua
+++ mylua/vyew.lua
@@ -1,6 +1,6 @@
 function Image(img)
-    if img.classes:find('epigraph',1) then
-      local f = io.open("quotes/" .. img.src, 'r')
+    if img.classes:find('vyew',1) then
+      local f = io.open("mycontribution/" .. img.src, 'r')
       local doc = pandoc.read(f:read('*a'))
       f:close()
       local caption = pandoc.utils.stringify(doc.meta.caption) 
