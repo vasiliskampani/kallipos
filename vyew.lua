e1bd7fbeee4308649e82a5f900064cd152f1eac8
vasiliskampani <72518208+vasiliskampani@users.noreply.github.com>
2022-11-19 01:24:09 +0200

    Update vyew.lua

--- vyew.lua
+++ vyew.lua
@@ -1,12 +1,13 @@
 function Image(img)
-    if img.classes:find('vyew',1) then
-      local f = io.open("mycontribution/" .. img.src, 'r')
-      local doc = pandoc.read(f:read('*a'))
-      f:close()
-      local caption = pandoc.utils.stringify(doc.meta.caption) 
-      local name = pandoc.utils.stringify(doc.meta.name)
-      local am = pandoc.utils.stringify(doc.meta.id)
-      local content = "> _" .. caption .. "  \n>" .. "Ονοματεπωνυμο Φοιτητη:" .. name .. "\nAριθμος Mητρωου:" .. am
-      return pandoc.RawInline('markdown',content)
-    end
+  if img.classes:find('vyew',1) then
+    local f = io.open("mycontribution/" .. img.src, 'r')
+    local doc = pandoc.read(f:read('*a'))
+    f:close()
+    local caption = pandoc.utils.stringify(doc.meta.caption) or "Epigraph has not been set"
+    local student = pandoc.utils.stringify(doc.meta.student) or "Student has not been set"
+    local studentid = pandoc.utils.stringify(doc.meta.studentid) or "Student ID has not been set"
+    local credentials = "Student: " .. student .. " (" .. studentid .. ")"
+    local comment = "> _" .. caption .. "_  \n> " .. "**" .. credentials .. "**"
+    return pandoc.RawInline('markdown',comment)
+  end
 end
